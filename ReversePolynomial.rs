1e2889c5e72e39074e5505eae409841430c06932
Pranavya <lomdom12345@gmail.com>
2017-09-23 18:02:57 +0530

    Fixed errors

---
+++ ReversePolynomial.rs
@@ -0,0 +1,19 @@
+fn main() {
+    use std::io::{stdin,stdout,Write};
+    let mut s=String::new();
+    print!("Enter polynomial: ");
+    let _=stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    let result = str::replace(&s, "-", "+-");
+    let joined = format!("{}{}","+", &result);
+    let l = s.split("");
+    let fs = "";
+    for i in l {
+        if i == "+" {
+            format!("{}{}", "+", &fs);
+        }
+        println!("{}{}", i, fs);
+    }
+    println!("{}", joined);
+
+}
